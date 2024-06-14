f19c36701f893e0fa3ba92334d797de128d49ff5
Harshvardhan Makwana <harshvardhanmakwana786@gmail.com>
2024-06-14 17:58:41 +0530

    Jest Setup Completed and Server Setup Completed

---
+++ server/test/integration/server.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+// import { SHUTDOWN } from '../../src/server.js';
+import { app } from '../../src/app.js';
+
+describe('Application', () => {
+    it('Starts and has the proper test environment', async () => {
+        expect(process.env.NODE_ENV).toBe('test');
+        expect(app).toBeDefined();
+    }, 10000);
+});
+
+// afterAll((done) => {
+//     SHUTDOWN(done);
+// });
+
+// it('Returns all options allowed when called from the HTTP method options', async () => {
+//     const response = await request(app).options('/');
+
+//     expect(response.status).toBe(200);
+//     expect(response.headers['access-control-allow-methods']).toBe('PUT, POST, PATCH, DELETE, GET');
+// }, 10000);
+
+// it('Returns 404 when the route requested is not found.', async () => {
+//     const response = await request(app).get('/a/cute/route/that/does/not/exist/');
+
+//     expect(response.status).toBe(404);
+// }, 10000);
